b65d7231d5a457da7c7b0515ed2f3a602a1a0aba
Mike Martinez <brillozon@gmail.com>
2015-01-02 23:20:03 -0600

    added more configurable settings. cleaned up look. removed experimental GL output. added confusion matrix settings and output.  made plot conditional

--- ui.R
+++ ui.R
@@ -13,30 +13,44 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("lambda",
-                  div(p("Lambda"),p("(Regularization Parameter)")),
+                  div(h4("Lambda:"),p("(Regularization Parameter)")),
                   min = 0.0, max = 10.0,
                   value = 1.0, step=0.1),
       hr(),
       wellPanel(
-        numericInput("degree", label = div(h4('Complexity'),p('[1-17]')),
+        sliderInput("degree", label = div(h6('Complexity:'),p('[1-17]')),
                      value = 6, min = 1, max = 17, step = 1),
-        numericInput("maxit", label = div(h4('Maximum Iterations'),p('[1-700]')),
+        sliderInput("maxit", label = div(h6('Maximum Iterations:'),p('[1-700]')),
                      value = 400, min = 1, max = 700, step = 1),
-        numericInput("gridlines", label = div(h4('Contour Gridlines'),p('[50-200]')),
+        sliderInput("gridlines", label = div(h6('Contour Gridlines:'),p('[50-200]')),
                      value = 100, min = 50, max = 200, step = 1),
-        textOutput("result$convergence")
-      )  # ,
-#       sliderInput("pts", 
-#                   "Number of points in 3d scatterplot:", 
-#                   min = 10, 
-#                   max = 1000, 
-#                   value = 250)
+        sliderInput("threshold", label = div(h6("Threshold:"),p('[0.0-1.0]')),
+                     value = 0.0, min = 0.0, max = 1.0, step = 0.01)
+      ),
+      span(HTML("Convergence:"),
+               textOutput("convergence",inline="true")),
+      p("(0 - converged"), p("1 - max iterations)")
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot",width="600px",height="600px"),
-      webGLOutput("sctPlot")
+      textOutput("currentParams"),
+      fluidRow(
+        column(4,
+               selectInput('dataset',"Confusion Matrix Dataset",
+                           c("Training" = 'training',"Validation" = 'validation'),
+                           selected='validation'),
+               htmlOutput("cfxtable")
+        ),
+        column(8,
+          htmlOutput("cfclass")
+        )
+      ),
+      hr(),
+      textOutput("plotParams"),
+      actionButton("replot",label="Replot Training Data"),
+      plotOutput("distPlot",width="600px",height="600px")
+      # , hr(), webGLOutput("sctPlot")
     )
   )
 ))
